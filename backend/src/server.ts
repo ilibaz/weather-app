c672ef1cb66ca10d7d1e5fcb5566941faeddfcfc
Ilia Bazanov <bazanov.ilia+git@gmail.com>
2024-04-21 21:21:35 +0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -36,7 +36,7 @@ app.get('/api/cities', (req: Request, res: Response) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'dist'));
+        res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'));
     });
 }
 
