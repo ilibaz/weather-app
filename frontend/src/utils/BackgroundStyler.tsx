77fab447b2f6f3eb8510529bc652b28e62820db9
Ilia Bazanov <bazanov.ilia+git@gmail.com>
2024-04-22 09:22:22 +0300

    improved effects

--- frontend/src/utils/BackgroundStyler.tsx
+++ frontend/src/utils/BackgroundStyler.tsx
@@ -14,21 +14,21 @@ const BackgroundStyler: React.FC<BackgroundStylerProps> = ({ children, weather }
         if (weather) {
             if (weather.condition === "cloudy") {
                 setBackgroundGradient('from-[#97d0f8] to-[#69bbf6]');
-                setAdditionalEffect("radial-gradient(circle at left top, rgb(200, 200, 200) 1%, transparent 30%)");
+                setAdditionalEffect("radial-gradient(circle at left top, rgb(200, 200, 200) 25%, transparent 75%)");
             } else if (weather.condition === "rain") {
                 setBackgroundGradient('from-[#dce2f6] to-[#c7d0ea]');
                 setAdditionalEffect("radial-gradient(circle at left top, rgb(25, 50, 155) 24%, transparent 100%)");
             } else if (weather.condition === "sunny") {
                 setBackgroundGradient('from-[#bfe3f4] to-[#93d6d1]');
-                setAdditionalEffect("radial-gradient(circle at left top, rgb(100, 200, 200) -15%, transparent 45%)");
+                setAdditionalEffect("radial-gradient(circle at left top, rgb(200, 200, 70) 5%, transparent 50%)");
             } else if (weather.condition === "windy") {
-                setBackgroundGradient('from-[#97d0f8] to-[#69bbf6]');
-                setAdditionalEffect("radial-gradient(circle at left top, rgb(200, 200, 200) 1%, transparent 30%)");
+                setBackgroundGradient('from-[#bbbbbb] to-[#cccccc]');
+                setAdditionalEffect("radial-gradient(circle at left top, rgb(250, 250, 250) 1%, transparent 30%)");
             }
         } else {
-            // default to sunny
-            setBackgroundGradient('from-[#bfe3f4] to-[#93d6d1]');
-            setAdditionalEffect("radial-gradient(circle at left top, rgb(100, 200, 200) -15%, transparent 45%)");
+            // default
+            setBackgroundGradient('bg-gray-300');
+            setAdditionalEffect("none");
         }
     }, [weather]);
 
